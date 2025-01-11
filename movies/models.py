cd0e1e9c6d309ff0c57285bd6ef8826af03fe552
Caleb San Lucas <cssan@puce.edu.ec>
2025-01-11 10:52:24 -0500

    ExamenParcial2

--- movies/models.py
+++ movies/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+
+class Movie(models.Model):
+    id = models.IntegerField(primary_key=True )
+    title_movie = models.CharField(max_length=30, null=False)
+    MOVIE_GENRES = {
+        ('A', 'Aventura'),
+        ('F', 'Ficcion'),
+        ('T', 'Thriller'),
+        ('R', 'Romance')
+    }
+    
+    movie_genre = models.CharField(max_length=30,choices=MOVIE_GENRES, null=False)
+    director = models.CharField(max_length=30, null=False)
+    release_year = models.DateField()
+    sinopsis = models.TextField()
+    
+    def __str__(self):
+        return self.title_movie 
